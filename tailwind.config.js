4036ed23add8d15afe80edbb44ef1d6f4291d0ad
Thales Piassi <thales.piassi.nunes@gmail.com>
2023-04-06 10:35:39 -0300

    feat: remove unused tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 // eslint-disable-next-line jsdoc/valid-types
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./pages/**/*.{php,js}',
-		'./components/**/*.{php,js}',
-		'./src/Pages/**/*.{php,js}',
-		'./src/Components/**/*.{php,js}',
-	],
+	content: ['./src/Pages/**/*.{php,js}', './src/Components/**/*.{php,js}'],
 	theme: {
 		container: {
 			center: true,
